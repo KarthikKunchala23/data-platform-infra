f4684bd315ce60907530db4f965fb0454bbca4e0
karthik <karthikkunchala170.com>
2025-09-12 09:58:21 +0530

    Addded kubernetes manifests using kuberenetes-alpha provider

--- terraform/envs/dev/provider.tf
+++ terraform/envs/dev/provider.tf
@@ -15,3 +15,11 @@ token = data.aws_eks_cluster_auth.this.token
 data "aws_eks_cluster_auth" "this" {
 name = module.eks.cluster_name
 }
+
+provider "kubernetes-alpha" {
+  alias = "alpha"
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  
+}
--- terraform/modules/rbac/main.tf
+++ terraform/modules/rbac/main.tf
@@ -1,23 +1,29 @@
-resource "kubernetes_config_map" "argocd_cm" {
-  metadata {
-    name      = "argocd-cm"
-    namespace = var.namespace
-  }
-
-  data = {
-    "accounts.${var.account_name}"         = var.account_capabilities
-    "accounts.${var.account_name}.enabled" = tostring(var.enable_account)
+resource "kubernetes_manifest" "argocd_cm_patch" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "ConfigMap"
+    metadata = {
+      name      = "argocd-cm"
+      namespace = var.namespace
+    }
+    data = {
+      "accounts.${var.account_name}"         = var.account_capabilities
+      "accounts.${var.account_name}.enabled" = tostring(var.enable_account)
+    }
   }
 }
 
-resource "kubernetes_config_map" "argocd_rbac_cm" {
-  metadata {
-    name      = "argocd-rbac-cm"
-    namespace = var.namespace
-  }
-
-  data = {
-    "policy.csv" = var.rbac_policies
-    "scopes"     = var.rbac_scopes
+resource "kubernetes_manifest" "argocd_rbac_cm_patch" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "ConfigMap"
+    metadata = {
+      name      = "argocd-rbac-cm"
+      namespace = var.namespace
+    }
+    data = {
+      "policy.csv" = var.rbac_policies
+      "scopes"     = var.rbac_scopes
+    }
   }
 }
