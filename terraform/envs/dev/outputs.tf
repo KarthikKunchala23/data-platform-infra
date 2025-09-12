24be8e7f8fb847982b22aae5babfb36ed5b4c86b
karthik <karthikkunchala170.com>
2025-09-12 11:53:02 +0530

    added irsa for airflow

--- terraform/envs/dev/outputs.tf
+++ terraform/envs/dev/outputs.tf
@@ -35,5 +35,5 @@ output "oidc_provider_arn" {
 }
 
 output "cluster_oidc_issuer" {
-  value = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+  value = data.tls_certificate.oidc.subjects[0].common_name
 }
