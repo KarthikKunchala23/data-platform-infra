18fe42c7bbc2406f231b58d823dc081f001144e0
karthik <karthikkunchala170.com>
2025-09-13 21:12:07 +0530

    fixed the iam policy attachment and helm

--- terraform/envs/dev/provider.tf
+++ terraform/envs/dev/provider.tf
@@ -13,6 +13,7 @@ token = data.aws_eks_cluster_auth.this.token
 
 provider "helm" {
   kubernetes ={
+    config_path = "~/.kube/config"
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
