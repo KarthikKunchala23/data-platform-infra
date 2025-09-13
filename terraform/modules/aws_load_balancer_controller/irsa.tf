38efb5ea4bf3b0d74369f2a7f003d64bd815f7f1
karthik <karthikkunchala170.com>
2025-09-13 21:12:21 +0530

    fixed the iam policy attachment and helm

--- terraform/modules/aws_load_balancer_controller/irsa.tf
+++ terraform/modules/aws_load_balancer_controller/irsa.tf
@@ -1,11 +1,9 @@
-data "http" "alb_controller_policy" {
-  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.1/docs/install/iam_policy.json"
-}
+
 
 resource "aws_iam_policy" "alb_controller" {
   name        = "${var.cluster_name}-alb-controller-policy"
   description = "Policy for AWS Load Balancer Controller"
-  policy      = data.http.alb_controller_policy.response_body
+  policy      = file("${path.module}/iam-policy.json")
 }
 
 
