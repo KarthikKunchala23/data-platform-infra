06d80f44a24261593073199c51e54fb0a842d823
karthik <karthikkunchala170.com>
2025-09-14 12:30:09 +0530

    added region and vpc in alb helm app

--- terraform/envs/dev/provider.tf
+++ terraform/envs/dev/provider.tf
@@ -11,13 +11,6 @@ cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_d
 token = data.aws_eks_cluster_auth.this.token
 }
 
-provider "helm" {
-kubernetes = {
-host = module.eks.cluster_endpoint
-cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-token = data.aws_eks_cluster_auth.this.token
-}
-}
 
 data "aws_eks_cluster_auth" "this" {
 name = module.eks.cluster_name
