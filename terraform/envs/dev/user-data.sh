ad15397cec2a6fa5bfe1efd11876219c63c4ee60
karthik <karthikkunchala170.com>
2025-10-12 09:03:08 +0530

    fixed user data script

--- terraform/envs/dev/user-data.sh
+++ terraform/envs/dev/user-data.sh
@@ -28,7 +28,7 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubectl
-kubectl version --client --short
+kubectl version --client
 
 # --------------------------------------------------
 # Install k9s (latest release)
